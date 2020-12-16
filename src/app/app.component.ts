a00f32c634f09f3ab351a4b5287e98070b69baad
dipali-shinde <shinde.dipali2607@gmail.com>
2020-12-16 17:41:54 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   name = 'my first app';
+  displayDetails = false;
+  buttonClickCount = 0;
+  ClickNumbersLog =[];
+  
+  onShowDeaiils()
+  {
+    this.displayDetails = !this.displayDetails;//toggle details
+    this.buttonClickCount++;//increase click count
+    this.ClickNumbersLog.push(this.buttonClickCount);// added click count to array
+    
+  }
+  getcolor(number){
+    if(number % 2 == 0){
+      return 'red';
+    } else{
+      return 'green';
+    }
+  }
 }
